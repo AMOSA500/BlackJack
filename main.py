c9e44f1fd21cea569963a2539fd4cddf68fb958d
AMOSA500 <66817828+AMOSA500@users.noreply.github.com>
2023-11-25 10:41:59 +0000

    User bank account integrated and updated on win or lose

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+BLACKJACK CARD GAME
+This game was referenced from - https://games.washingtonpost.com/games/blackjack
+"""
 import random
 
 from art import logo
@@ -7,14 +11,15 @@ print(logo)
 # Variable declaration
 user_score = 0 
 computer_score = 0
-user_ = 0
-comp_wins = 0
+user_bank = 1000
+comp_bank = 0
 user_cards = []
 computer_cards = []
 
 def deal_cards():
   cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
-  return random.choice(sorted(cards))
+  random.shuffle(cards)
+  return random.choice(cards)
   
 
 # Sum Cards
@@ -33,41 +38,66 @@ def sum_cards(cards):
 
 
 # Compare cards
-def compare_score(user_score,computer_score):
-  global user_wins, comp_wins
+def compare_score(user_score,computer_score,price):
+  global user_bank, comp_bank
   if user_score == computer_score:
     return f'Push, Draw - {user_score} == {computer_score}'
   elif computer_score == 0:
-    comp_wins += 1
+    comp_bank += price
+    user_bank -= price
     return 'Lose, Computer has BlackJack'
   elif user_score == 0:
-    user_wins += 1
+    comp_bank -= price
+    user_bank += price
     return 'Win, User has BlackJack'
   elif user_score > 21:
-    comp_wins += 1
+    comp_bank += price
+    user_bank -= price
     return 'User went over, You lose'
   elif computer_score > 21:
-    user_wins +=1 
+    comp_bank -= price
+    user_bank += price
     return 'Computer went over, User win'
   elif user_score > computer_score:
-    user_wins += 1
+    comp_bank -= price
+    user_bank += price
     return 'User win'
   else:
-    comp_wins += 1
+    comp_bank += price
+    user_bank -= price
     return 'Computer Win'
 
+# Bank printing room
+def bank_deal():
+  price_list = [1,5,25,50,100,500,1000]
+  check = False
+  while not check:
+    price = int(input(f'Enter your deals within {price_list}: \n£'))
+    if price in price_list:
+      check = True
+      return price
+    else:
+      print(f'{price} is not in bank notes')
+
+# Divider
+def page_divider():
+  print('-'*30)
 
 
 # Card Starter
 def play_game():
-  global user_score, computer_score
+  global user_score, computer_score,user_bank,comp_bank
 
+  # Deals
+  price = bank_deal()
+  game_over = False
+  
   # Hand first two cards to player
   for _ in range(2):
     user_cards.append(deal_cards())
     computer_cards.append(deal_cards())
-    
-  game_over = False
+
+  # Keep game running
   while not game_over:
     # Display cards
     print(f'User Cards: {user_cards} = {sum_cards(user_cards)}')
@@ -93,21 +123,28 @@ def play_game():
       computer_score = sum_cards(computer_cards)
 
   # Display score board
+  page_divider()
   print(f'User final hand {user_cards}, final score {user_score}')
   print(f'Computer final hand {computer_cards}, final score {computer_score}')
-  result = compare_score(user_score,computer_score)
+  result = compare_score(user_score,computer_score,price=price)
   print(result)
+  page_divider()
+  page_divider()
+  print(f'User Bank Balance: £{user_bank}')
+  
 
   # Divider
-  print('-'*10)
+  page_divider()
   restart = input('Restart the game, Type "Y" or "N": ').lower().strip()
-  if restart == 'y':
+  if restart == 'y' and user_bank > 0:
     user_cards.clear()
     computer_cards.clear()
     user_score = computer_score = 0
     play_game()
-  else:
+  elif restart == 'n':
     print('Goodbye')
+  else:
+    print('Low Bank balance, house wins')
 
 play_game()
 
